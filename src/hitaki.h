a86573dff2be6cc0f6d03bfcf563a096f3810867
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-04-29 11:25:03 +0900

    start hitaki project under LGPL v2.1 or later
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/hitaki.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#ifndef __HITAKI_H__
+#define __HITAKI_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include <hitaki_sigs_marshal.h>
+
+#include <hitaki_enum_types.h>
+#include <hitaki_enums.h>
+
+#endif
---
+++ src/hitaki_enum_types.h
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#ifndef __HITAKI_ENUM_TYPES_H__
+#define __HITAKI_ENUM_TYPES_H__
+
+#endif
