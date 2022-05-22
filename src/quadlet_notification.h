6b32c2aa61f2e2f2be320b4e7e09557a4ac833c7
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-05-22 10:51:00 +0900

    quadlet_notification: add GObject interface for notification with quadlet mesasge
    Some of sound unit in IEEE 1394 bus support notification with quadlet
    message.
    
    This commit adds GObject Interface to handle the message.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/quadlet_notification.h
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#ifndef __HITAKI_QUADLET_NOTIFICATION_H__
+#define __HITAKI_QUADLET_NOTIFICATION_H__
+
+#include <hitaki.h>
+
+G_BEGIN_DECLS
+
+#define HITAKI_TYPE_QUADLET_NOTIFICATION    (hitaki_quadlet_notification_get_type())
+
+G_DECLARE_INTERFACE(HitakiQuadletNotification, hitaki_quadlet_notification, HITAKI, QUADLET_NOTIFICATION, GObject);
+
+struct _HitakiQuadletNotificationInterface {
+    GTypeInterface iface;
+
+    /**
+     * HitakiQuadletNotificationInterface::notified:
+     * @self: A [iface@QuadletNotification]
+     * @message: A quadlet message in notification.
+     *
+     * Class closure for the [signal@QuadletNotification::notified] signal.
+     */
+    void (*notified)(HitakiQuadletNotification *self, guint32 message);
+};
+
+G_END_DECLS
+
+#endif
