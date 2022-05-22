7d594800309a07d48d6882c3d56a7e068a1d9ee5
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-05-22 10:51:00 +0900

    enums: add enumerations for type of sound unit
    In UAPI of ALSA firewire stack, the type of sound unit is defined.
    
    This patchset adds GObject enumerations to express the type.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/hitaki_enum_types.h
+++ src/hitaki_enum_types.h
@@ -2,4 +2,28 @@
 #ifndef __HITAKI_ENUM_TYPES_H__
 #define __HITAKI_ENUM_TYPES_H__
 
+/**
+ * HitakiAlsaFirewireType:
+ * @HITAKI_ALSA_FIREWIRE_TYPE_DICE:      The type of DICE ASIC.
+ * @HITAKI_ALSA_FIREWIRE_TYPE_FIREWORKS: The type of Fireworks board module.
+ * @HITAKI_ALSA_FIREWIRE_TYPE_BEBOB:     The type of BeBoB ASIC.
+ * @HITAKI_ALSA_FIREWIRE_TYPE_OXFW:      The type of OXFW ASIC
+ * @HITAKI_ALSA_FIREWIRE_TYPE_DIGI00X:   The type of Digi00x series.
+ * @HITAKI_ALSA_FIREWIRE_TYPE_TASCAM:    The type of Tascam FireWire series.
+ * @HITAKI_ALSA_FIREWIRE_TYPE_MOTU:      The type of MOTU FireWire series.
+ * @HITAKI_ALSA_FIREWIRE_TYPE_FIREFACE:  The type of RME Fireface series.
+ *
+ * The enumerations for type of sound unit defined by ALSA firewire stack.
+ */
+typedef enum {
+    HITAKI_ALSA_FIREWIRE_TYPE_DICE = 1,
+    HITAKI_ALSA_FIREWIRE_TYPE_FIREWORKS,
+    HITAKI_ALSA_FIREWIRE_TYPE_BEBOB,
+    HITAKI_ALSA_FIREWIRE_TYPE_OXFW,
+    HITAKI_ALSA_FIREWIRE_TYPE_DIGI00X,
+    HITAKI_ALSA_FIREWIRE_TYPE_TASCAM,
+    HITAKI_ALSA_FIREWIRE_TYPE_MOTU,
+    HITAKI_ALSA_FIREWIRE_TYPE_FIREFACE,
+} HitakiAlsaFirewireType;
+
 #endif
