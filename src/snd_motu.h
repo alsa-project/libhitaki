c11da71a2278f1f1a1658d669d9cc4f5d877eaab
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-05-24 14:22:16 +0900

    snd_motu: add object for sound unit of MOTU FireWire series
    This commit adds GObject-derived object which implements
    Hitaki.AlsaFirewire, Hitaki.QuadletNotification, Hitaki.MotuRegisterDsp,
    and Hitaki.MotuCommandDsp interfaces for sound unit in MOTU FireWire
    series.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/snd_motu.h
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#ifndef __HITAKI_SND_MOTU_H__
+#define __HITAKI_SND_MOTU_H__
+
+#include <hitaki.h>
+
+G_BEGIN_DECLS
+
+#define HITAKI_TYPE_SND_MOTU        (hitaki_snd_motu_get_type())
+
+G_DECLARE_DERIVABLE_TYPE(HitakiSndMotu, hitaki_snd_motu, HITAKI, SND_MOTU, GObject);
+
+struct _HitakiSndMotuClass {
+    GObjectClass parent_class;
+};
+
+HitakiSndMotu *hitaki_snd_motu_new(void);
+
+G_END_DECLS
+
+#endif
